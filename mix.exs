4cfcdd9c51f89106c26d0e5187a88c3e6fcaaac8
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-06-06 12:03:22 -0300

    build(deps): supports elixir/erlang 1.14/24

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule Tower.MixProject do
       app: :tower,
       description: @description,
       version: @version,
-      elixir: "~> 1.15",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
