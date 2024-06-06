02b38d677fb3be99f12d41b5ebc1fb1c0eb21486
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-06-06 14:45:40 -0300

    build(deps): supports elixir/erlang 1.14/24 (#8)

--- lib/tower/logger_handler.ex
+++ lib/tower/logger_handler.ex
@@ -24,6 +24,7 @@ defmodule Tower.LoggerHandler do
     :ok
   end
 
+  # elixir 1.15+
   def log(%{level: :error, meta: %{crash_reason: {exception, stacktrace}} = meta}, _config)
       when is_exception(exception) and is_list(stacktrace) do
     IO.puts("[Tower.LoggerHandler] EXCEPTION #{inspect(exception)}")
@@ -31,6 +32,7 @@ defmodule Tower.LoggerHandler do
     Tower.report_exception(exception, stacktrace, meta)
   end
 
+  # elixir 1.15+
   def log(
         %{level: :error, meta: %{crash_reason: {{:nocatch, reason}, stacktrace}} = meta},
         _config
@@ -41,11 +43,60 @@ defmodule Tower.LoggerHandler do
     Tower.report(:nocatch, reason, stacktrace, meta)
   end
 
+  # elixir 1.15+
   def log(%{level: :error, meta: %{crash_reason: {exit_reason, stacktrace}} = meta}, _config)
       when is_list(stacktrace) do
     IO.puts("[Tower.LoggerHandler] EXIT #{inspect(exit_reason)}")
 
     Tower.report(:exit, exit_reason, stacktrace, meta)
+  end
+
+  # elixir 1.14
+  def log(
+        %{
+          level: :error,
+          msg: {:report, %{report: %{reason: {exception, stacktrace}}}},
+          meta: meta
+        },
+        _config
+      )
+      when is_exception(exception) and is_list(stacktrace) do
+    Tower.report_exception(exception, stacktrace, meta)
+  end
+
+  # elixir 1.14
+  def log(
+        %{
+          level: :error,
+          msg: {:report, %{report: %{reason: {{:nocatch, reason}, stacktrace}}}},
+          meta: meta
+        },
+        _config
+      )
+      when is_list(stacktrace) do
+    Tower.report(:nocatch, reason, stacktrace, meta)
+  end
+
+  # elixir 1.14
+  def log(
+        %{
+          level: :error,
+          msg: {:report, %{report: %{reason: {reason, stacktrace}}}},
+          meta: meta
+        },
+        _config
+      )
+      when is_list(stacktrace) do
+    case Exception.normalize(:error, reason) do
+      %ErlangError{} ->
+        Tower.report(:exit, reason, stacktrace, meta)
+
+      e when is_exception(e) ->
+        Tower.report_exception(e, stacktrace, meta)
+
+      _ ->
+        Tower.report(:exit, reason, stacktrace, meta)
+    end
   end
 
   def log(log_event, _config) do
