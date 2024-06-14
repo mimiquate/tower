f423b390330feb3786ae09b8d760aff397f708e3
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-06-14 16:01:44 -0300

    mix format

--- lib/tower/application.ex
+++ lib/tower/application.ex
@@ -8,9 +8,7 @@ defmodule Tower.Application do
   @impl true
   def start(_type, _args) do
     [
-      {Task.Supervisor, name: Tower.TaskSupervisor},
-      # Starts a worker by calling: Tower.Worker.start_link(arg)
-      # {Tower.Worker, arg}
+      {Task.Supervisor, name: Tower.TaskSupervisor}
     ]
     |> Supervisor.start_link(strategy: :one_for_one, name: Tower.Supervisor)
   end
