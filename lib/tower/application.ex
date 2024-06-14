b35505e0b95ec5e3dc31b5baa8997b3aab9b2c7c
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-06-14 15:57:01 -0300

    avoid unnecessary local variables

--- lib/tower/application.ex
+++ lib/tower/application.ex
@@ -7,14 +7,10 @@ defmodule Tower.Application do
 
   @impl true
   def start(_type, _args) do
-    children = [
+    [
       # Starts a worker by calling: Tower.Worker.start_link(arg)
       # {Tower.Worker, arg}
     ]
-
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Tower.Supervisor]
-    Supervisor.start_link(children, opts)
+    |> Supervisor.start_link(strategy: :one_for_one, name: Tower.Supervisor)
   end
 end
