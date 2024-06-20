246f017328590748b48d4a75cde7bda63f9515bf
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-06-20 11:49:37 -0300

    mix format

--- lib/tower/reporter.ex
+++ lib/tower/reporter.ex
@@ -3,7 +3,8 @@ defmodule Tower.Reporter do
   Reports an exception.
   """
   @callback report_exception(exception :: Exception.t(), stacktrace :: list()) :: :ok
-  @callback report_exception(exception :: Exception.t(), stacktrace :: list(), metadata :: map()) :: :ok
+  @callback report_exception(exception :: Exception.t(), stacktrace :: list(), metadata :: map()) ::
+              :ok
   @callback report_term(reason :: term()) :: :ok
   @callback report_term(reason :: term(), metadata :: map()) :: :ok
 end
