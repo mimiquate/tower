6cb0a7b751323f60de138021d3ce3b0e5641adde
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-06-20 11:02:16 -0300

    feat: improves Tower.Reporter behaviour typespec

--- lib/tower/reporter.ex
+++ lib/tower/reporter.ex
@@ -2,5 +2,12 @@ defmodule Tower.Reporter do
   @doc """
   Reports an exception.
   """
-  @callback report_exception(Exception.t(), list()) :: :ok
+  @callback report_exception(exception :: Exception.t(), stacktrace :: list()) :: :ok
+  @callback report_exception(
+              exception :: Exception.t(),
+              stacktrace :: list(),
+              options :: Keyword.t()
+            ) :: :ok
+  @callback report_message(message :: String.t()) :: :ok
+  @callback report_message(message :: String.t(), options :: Keyword.t()) :: :ok
 end
