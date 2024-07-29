483f7cd0c8f63acb63ecb51fb53c37a9d17e8d0b
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-07-29 14:03:57 -0300

    Tower.Event.t()

--- lib/tower/event.ex
+++ lib/tower/event.ex
@@ -1,6 +1,13 @@
 defmodule Tower.Event do
   defstruct [:kind, :reason, :stacktrace, :log_event_meta]
 
+  @type t :: %__MODULE__{
+          kind: :error | :exit | :throw,
+          reason: Exception.t() | term(),
+          stacktrace: Exception.stacktrace(),
+          log_event_meta: :logger.metadata()
+        }
+
   def from_exception(exception, stacktrace, log_event_meta) do
     %__MODULE__{
       kind: :error,
