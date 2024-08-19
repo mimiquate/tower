8956e263e61c6649fbbc63b166921504cad0828e
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-08-19 17:26:03 -0300

    refactor: don't be unnecessarily too strict about dependencies compatible version range

--- mix.exs
+++ mix.exs
@@ -40,8 +40,8 @@ defmodule Tower.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:uniq, "~> 0.6.1"},
-      {:telemetry, "~> 1.2"},
+      {:uniq, "~> 0.6.0"},
+      {:telemetry, "~> 1.1"},
 
       # Dev
       {:ex_doc, "~> 0.34.0", only: :dev, runtime: false},
