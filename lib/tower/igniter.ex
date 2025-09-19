c047ff0dff178ecc55f5d597d5c2a5e9c66a3817
Gonzalo R <456459+grzuy@users.noreply.github.com>
2025-09-19 15:32:02 -0300

    feat(igniter): `Tower.Igniter.runtime_configure_reporter/3` (#165)

--- lib/tower/igniter.ex
+++ lib/tower/igniter.ex
@@ -1,14 +1,82 @@
 if Code.ensure_loaded?(Igniter) do
   defmodule Tower.Igniter do
-    def add_reporter_to_config(igniter, reporter) do
-      igniter
-      |> Igniter.Project.Config.configure(
+    @runtime_file_path "config/runtime.exs"
+    @default_runtime_file_content """
+    import Config
+    """
+
+    def reporters_list_append(igniter, module) do
+      Igniter.Project.Config.configure(
+        igniter,
         "config.exs",
         :tower,
         [:reporters],
-        [reporter],
-        updater: &Igniter.Code.List.append_new_to_list(&1, reporter)
+        [module],
+        updater: &Igniter.Code.List.append_new_to_list(&1, module)
       )
+    end
+
+    def runtime_configure_reporter(igniter, application, items, opts \\ []) do
+      env = opts[:env]
+
+      igniter
+      |> Igniter.include_or_create_file(@runtime_file_path, @default_runtime_file_content)
+      |> Igniter.update_elixir_file(
+        @runtime_file_path,
+        fn zipper ->
+          if Igniter.Project.Config.configures_root_key?(zipper, application) do
+            {:ok, zipper}
+          else
+            if env do
+              zipper
+              |> Igniter.Code.Common.move_to_cursor_match_in_scope(env_config_patterns(env))
+            else
+              {:ok, zipper}
+            end
+            |> case do
+              {:ok, zipper} ->
+                if Igniter.Project.Config.configures_root_key?(zipper, application) do
+                  {:ok, zipper}
+                else
+                  Igniter.Code.Common.add_code(zipper, config_block(application, items))
+                end
+
+              :error ->
+                Igniter.Code.Common.add_code(zipper, env_config_block(env, application, items))
+            end
+          end
+        end
+      )
+    end
+
+    defp env_config_block(env, application, config) do
+      """
+      if config_env() == #{inspect(env)} do
+      #{config_block(application, config)}
+      end
+      """
+    end
+
+    defp config_block(application, config) do
+      """
+      config #{inspect(application)},
+        #{Enum.map_join(config, ",\n", fn {key, value} -> "#{key}: #{value}" end)}
+      """
+    end
+
+    defp env_config_patterns(env) do
+      [
+        """
+        if config_env() == #{inspect(env)} do
+          __cursor__()
+        end
+        """,
+        """
+        if #{inspect(env)} == config_env() do
+          __cursor__()
+        end
+        """
+      ]
     end
   end
 end
---
+++ test/tower/igniter_test.exs
@@ -0,0 +1,225 @@
+defmodule TowerIgniterTest do
+  use ExUnit.Case, async: true
+  import Igniter.Test
+
+  describe "reporters_list_append/2" do
+    test "from scratch" do
+      test_project()
+      |> Tower.Igniter.reporters_list_append(Reporter)
+      |> assert_creates(
+        "config/config.exs",
+        """
+        import Config
+        config :tower, reporters: [Reporter]
+        """
+      )
+    end
+
+    test "appends to list if existing config" do
+      test_project(
+        files: %{
+          "config/config.exs" => """
+          import Config
+
+          config :tower, reporters: [ReporterOne]
+          """
+        }
+      )
+      |> Tower.Igniter.reporters_list_append(ReporterTwo)
+      |> assert_has_patch(
+        "config/config.exs",
+        """
+        |import Config
+        |
+        - |config :tower, reporters: [ReporterOne]
+        + |config :tower, reporters: [ReporterOne, ReporterTwo]
+        """
+      )
+    end
+
+    test "does nothing if module already listed" do
+      test_project(
+        files: %{
+          "config/config.exs" => """
+          import Config
+
+          config :tower, reporters: [ReporterOne, ReporterTwo]
+          """
+        }
+      )
+      |> Tower.Igniter.reporters_list_append(ReporterTwo)
+      |> assert_unchanged()
+    end
+
+    test "is idempotent" do
+      test_project()
+      |> Tower.Igniter.reporters_list_append(Reporter)
+      |> apply_igniter!()
+      |> Tower.Igniter.reporters_list_append(Reporter)
+      |> assert_unchanged()
+    end
+  end
+
+  describe "runtime_configure_reporter/4" do
+    test "from scratch" do
+      test_project()
+      |> Tower.Igniter.runtime_configure_reporter(
+        :reporter,
+        api_key: ~s[System.get_env("REPORTER_API_KEY")]
+      )
+      |> assert_creates(
+        "config/runtime.exs",
+        """
+        import Config
+        config :reporter, api_key: System.get_env("REPORTER_API_KEY")
+        """
+      )
+    end
+
+    test "from scratch with env" do
+      test_project()
+      |> Tower.Igniter.runtime_configure_reporter(
+        :reporter,
+        [api_key: ~s[System.get_env("REPORTER_API_KEY")]],
+        env: :prod
+      )
+      |> assert_creates(
+        "config/runtime.exs",
+        """
+        import Config
+
+        if config_env() == :prod do
+          config :reporter, api_key: System.get_env("REPORTER_API_KEY")
+        end
+        """
+      )
+    end
+
+    test "appends to existing runtime config" do
+      test_project(
+        files: %{
+          "config/runtime.exs" => """
+          import Config
+          """
+        }
+      )
+      |> Tower.Igniter.runtime_configure_reporter(
+        :reporter,
+        [api_key: ~s[System.get_env("REPORTER_API_KEY")]],
+        env: :prod
+      )
+      |> assert_has_patch(
+        "config/runtime.exs",
+        """
+        |import Config
+        |
+        + |if config_env() == :prod do
+        + |  config :reporter, api_key: System.get_env("REPORTER_API_KEY")
+        + |end
+        + |
+        """
+      )
+    end
+
+    test "writes inside existing prod block if present" do
+      test_project(
+        files: %{
+          "config/runtime.exs" => """
+          import Config
+
+          if config_env() == :prod do
+            IO.puts("hello")
+          end
+          """
+        }
+      )
+      |> Tower.Igniter.runtime_configure_reporter(
+        :reporter,
+        [api_key: ~s[System.get_env("REPORTER_API_KEY")]],
+        env: :prod
+      )
+      |> assert_has_patch(
+        "config/runtime.exs",
+        """
+        |if config_env() == :prod do
+        |  IO.puts("hello")
+        + |config :reporter, api_key: System.get_env("REPORTER_API_KEY")
+        |end
+        |
+        """
+      )
+    end
+
+    test "does not modify existing configs if config_env() == :prod block exists" do
+      test_project(
+        files: %{
+          "config/runtime.exs" => """
+          import Config
+
+          if config_env() == :prod do
+            config :reporter, api_key: System.get_env("REPORTER_API_KEY")
+          end
+          """
+        }
+      )
+      |> Tower.Igniter.runtime_configure_reporter(
+        :reporter,
+        [api_key: ~s[System.get_env("REPORTER_API_KEY")]],
+        env: :prod
+      )
+      |> assert_unchanged()
+    end
+
+    test "does not modify existing configs despite different" do
+      test_project(
+        files: %{
+          "config/runtime.exs" => """
+          import Config
+
+          if config_env() == :prod do
+            config :reporter, stale_key: System.get_env("REPORETER_STALE_KEY")
+          end
+          """
+        }
+      )
+      |> Tower.Igniter.runtime_configure_reporter(
+        :reporter,
+        [api_key: ~s[System.get_env("REPORTER_API_KEY")]],
+        env: :prod
+      )
+      |> assert_unchanged()
+    end
+
+    test "does not modify existing configs exists" do
+      test_project(
+        files: %{
+          "config/runtime.exs" => """
+          import Config
+
+          config :reporter, api_key: System.get_env("REPORTER_API_KEY")
+          """
+        }
+      )
+      |> Tower.Igniter.runtime_configure_reporter(
+        :reporter,
+        [api_key: ~s[System.get_env("REPORTER_API_KEY")]],
+        env: :prod
+      )
+      |> assert_unchanged()
+    end
+
+    test "is idempotent" do
+      test_project()
+      |> Tower.Igniter.runtime_configure_reporter(
+        :reporter,
+        api_key: ~s[System.get_env("REPORTER_API_KEY")]
+      )
+      |> apply_igniter!()
+      |> Tower.Igniter.runtime_configure_reporter(
+        :reporter,
+        api_key: ~s[System.get_env("REPORTER_API_KEY")]
+      )
+      |> assert_unchanged()
+    end
+  end
+end
