e6b5208baf025ce1359ab6d7ec3704874a07492c
Gonzalo <456459+grzuy@users.noreply.github.com>
2025-10-24 15:58:26 -0300

    test with handle_sasl_reports true

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,18 @@
 ExUnit.start()
+
+new_config =
+  Application.get_all_env(:logger)
+  |> Keyword.put(:handle_sasl_reports, true)
+
+Application.put_all_env(logger: new_config)
+
+Logger.App.stop()
+
+# Shutdown the application
+Logger.App.stop()
+
+# And start it without warnings
+Application.put_env(:logger, :level, :error)
+Application.start(:logger)
+Application.delete_env(:logger, :level)
+Logger.configure(level: :debug)
