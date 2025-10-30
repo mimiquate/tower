860469d2ef5920b7f09b49e32c7d1400783caba3
Gonzalo <456459+grzuy@users.noreply.github.com>
2025-10-30 11:28:26 -0300

    refactor: process label off of reported pid, not self
    technically they should always match, but just in case

--- lib/tower/event.ex
+++ lib/tower/event.ex
@@ -135,7 +135,7 @@ defmodule Tower.Event do
       plug_conn: plug_conn(options),
       metadata:
         %{application: application_data_from_log_event(log_event)}
-        |> Map.merge(maybe_process_label())
+        |> Map.merge(maybe_process_label(log_event))
         |> Map.merge(logger_metadata(log_event))
         |> Map.merge(Keyword.get(options, :metadata, %{})),
       by: Keyword.get(options, :by)
@@ -175,14 +175,16 @@ defmodule Tower.Event do
   end
 
   if function_exported?(:proc_lib, :get_label, 1) do
-    defp maybe_process_label do
-      case :proc_lib.get_label(self()) do
+    defp maybe_process_label(%{meta: %{pid: pid}}) do
+      case :proc_lib.get_label(pid) do
         :undefined -> %{}
         process_label -> %{process_label: process_label}
       end
     end
+
+    defp maybe_process_label(_log_event), do: %{}
   else
-    defp maybe_process_label, do: %{}
+    defp maybe_process_label(_log_event), do: %{}
   end
 
   defp logger_metadata_keys do
