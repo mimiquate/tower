a14f508ccf822fb7527b72e18475354d1da637c6
Gonzalo <456459+grzuy@users.noreply.github.com>
2025-11-28 10:23:58 -0300

    test: better gen server metadata assertions

--- test/tower/gen_server_test.exs
+++ test/tower/gen_server_test.exs
@@ -57,7 +57,7 @@ defmodule TowerGenServerTest do
   test "reports bad return exit if GenServer callback throws" do
     capture_log(fn ->
       in_unlinked_process(fn ->
-        {:ok, pid} = GenServer.start(TestGenServer, [])
+        {:ok, pid} = GenServer.start(TestGenServer, [], name: MyTestGenServer)
         GenServer.cast(pid, {:throw, "something"})
       end)
     end)
@@ -69,10 +69,30 @@ defmodule TowerGenServerTest do
           kind: :exit,
           reason: {:bad_return_value, "something"},
           stacktrace: [],
+          metadata: %{
+            process:
+              %{
+                pid: _pid,
+                registered_name: MyTestGenServer
+              } = process_metadata
+          },
           by: Tower.LoggerHandler
         }
       ] = reported_events()
     )
+
+    if Version.match?(System.version(), ">= 1.19.0") do
+      assert %{
+               gen_server: %{
+                 name: MyTestGenServer,
+                 last_message: {:"$gen_cast", {:throw, "something"}}
+               }
+             } = process_metadata
+    end
+
+    if Version.match?(System.version(), ">= 1.17.0") and System.otp_release() >= "27" do
+      assert %{process_label: {TestGenServer, init_args: []}} = process_metadata
+    end
   end
 
   test "doesn't report if GenServer callback exits normally" do
@@ -111,7 +131,7 @@ defmodule TowerGenServerTest do
   test "reports if GenServer callback exits abnormally" do
     capture_log(fn ->
       in_unlinked_process(fn ->
-        {:ok, pid} = GenServer.start(TestGenServer, [])
+        {:ok, pid} = GenServer.start(TestGenServer, [], name: MyTestGenServer)
         GenServer.cast(pid, {:exit, :abnormal})
       end)
     end)
@@ -123,16 +143,36 @@ defmodule TowerGenServerTest do
           kind: :exit,
           reason: :abnormal,
           stacktrace: [{TestGenServer, :handle_cast, 2, _} | _],
+          metadata: %{
+            process:
+              %{
+                pid: _pid,
+                registered_name: MyTestGenServer
+              } = process_metadata
+          },
           by: Tower.LoggerHandler
         }
       ] = reported_events()
     )
+
+    if Version.match?(System.version(), ">= 1.19.0") do
+      assert %{
+               gen_server: %{
+                 name: MyTestGenServer,
+                 last_message: {:"$gen_cast", {:exit, :abnormal}}
+               }
+             } = process_metadata
+    end
+
+    if Version.match?(System.version(), ">= 1.17.0") and System.otp_release() >= "27" do
+      assert %{process_label: {TestGenServer, init_args: []}} = process_metadata
+    end
   end
 
   test "reports if GenServer callback stops abnormally" do
     capture_log(fn ->
       in_unlinked_process(fn ->
-        {:ok, pid} = GenServer.start(TestGenServer, [])
+        {:ok, pid} = GenServer.start(TestGenServer, [], name: MyTestGenServer)
         GenServer.cast(pid, {:stop, :abnormal})
       end)
     end)
@@ -144,16 +184,36 @@ defmodule TowerGenServerTest do
           kind: :exit,
           reason: :abnormal,
           stacktrace: [],
+          metadata: %{
+            process:
+              %{
+                pid: _pid,
+                registered_name: MyTestGenServer
+              } = process_metadata
+          },
           by: Tower.LoggerHandler
         }
       ] = reported_events()
     )
+
+    if Version.match?(System.version(), ">= 1.19.0") do
+      assert %{
+               gen_server: %{
+                 name: MyTestGenServer,
+                 last_message: {:"$gen_cast", {:stop, :abnormal}}
+               }
+             } = process_metadata
+    end
+
+    if Version.match?(System.version(), ">= 1.17.0") and System.otp_release() >= "27" do
+      assert %{process_label: {TestGenServer, init_args: []}} = process_metadata
+    end
   end
 
   test "reports if GenServer stops abnormally" do
     capture_log(fn ->
       in_unlinked_process(fn ->
-        {:ok, pid} = GenServer.start(TestGenServer, [])
+        {:ok, pid} = GenServer.start(TestGenServer, [], name: MyTestGenServer)
         GenServer.stop(pid, :abnormal)
       end)
     end)
@@ -165,16 +225,36 @@ defmodule TowerGenServerTest do
           kind: :exit,
           reason: :abnormal,
           stacktrace: [],
+          metadata: %{
+            process:
+              %{
+                pid: _pid,
+                registered_name: MyTestGenServer
+              } = process_metadata
+          },
           by: Tower.LoggerHandler
         }
       ] = reported_events()
     )
+
+    if Version.match?(System.version(), ">= 1.19.0") do
+      assert %{
+               gen_server: %{
+                 name: MyTestGenServer,
+                 last_message: []
+               }
+             } = process_metadata
+    end
+
+    if Version.match?(System.version(), ">= 1.17.0") and System.otp_release() >= "27" do
+      assert %{process_label: {TestGenServer, init_args: []}} = process_metadata
+    end
   end
 
   test "reports two events when both GenServer terminates abnormally and client exits" do
     capture_log(fn ->
       in_unlinked_process(fn ->
-        {:ok, pid} = GenServer.start(TestGenServer, [])
+        {:ok, pid} = GenServer.start(TestGenServer, [], name: MyTestGenServer)
         # Client also raises because it doesn't receive a response from call
         GenServer.call(pid, {:stop, :abnormal})
       end)
@@ -188,6 +268,9 @@ defmodule TowerGenServerTest do
           kind: :exit,
           reason: {:abnormal, {GenServer, :call, _args}},
           stacktrace: [_ | _],
+          metadata: %{
+            process: %{pid: _client_pid}
+          },
           by: Tower.LoggerHandler
         },
         # server exit
@@ -196,10 +279,30 @@ defmodule TowerGenServerTest do
           kind: :exit,
           reason: :abnormal,
           stacktrace: [],
+          metadata: %{
+            process:
+              %{
+                pid: _server_pid,
+                registered_name: MyTestGenServer
+              } = process_metadata
+          },
           by: Tower.LoggerHandler
         }
       ] = reported_events()
     )
+
+    if Version.match?(System.version(), ">= 1.19.0") do
+      assert %{
+               gen_server: %{
+                 name: MyTestGenServer,
+                 last_message: {:stop, :abnormal}
+               }
+             } = process_metadata
+    end
+
+    if Version.match?(System.version(), ">= 1.17.0") and System.otp_release() >= "27" do
+      assert %{process_label: {TestGenServer, init_args: []}} = process_metadata
+    end
   end
 
   defp in_unlinked_process(fun) when is_function(fun, 0) do
